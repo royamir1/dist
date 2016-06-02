9cfe519670d21b2dd8777d063084176525206c5c
Roy Amir <roy.amir1@gmail.com>
2016-06-02 16:43:21 +0300

    build

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -5,8 +5,8 @@
 module.exports = {
   // Server IP
   ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+            process.env.IP  ||
+            '0.0.0.0',
 
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
@@ -16,7 +16,6 @@ module.exports = {
   // MongoDB connection options
   mongo: {
     uri:    process.env.MONGOLAB_URI ||
-            process.env.MONGOHQ_URL ||
             process.env.MONGODB_URI ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/sflypanel'
